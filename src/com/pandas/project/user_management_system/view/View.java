fa96a4e8038efccaa75912ece21ee02d178ed208
wangjing <wj2856379842@163.com>
2020-08-13 16:39:51 +0800

    用户管理系统项目包

--- src/com/pandas/project/user_management_system/view/View.java
+++ src/com/pandas/project/user_management_system/view/View.java
@@ -1,7 +1,5 @@
 package com.pandas.project.user_management_system.view;
 
-import com.pandas.project.user_management_system.controller.LoginController;
-
 /**
  * @author wangjing
  * @create 2020-08-13 16:04
