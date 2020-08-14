10471b3e491e937329d3d91c5c9767ed1bb28cc8
wangjing <wj2856379842@163.com>
2020-08-14 13:32:43 +0800

    不再修改系列

--- src/com/pandas/project/user_management_system/util/NumberUtil.java
+++ src/com/pandas/project/user_management_system/util/NumberUtil.java
@@ -17,23 +17,28 @@ public class NumberUtil {
       */
     public static boolean checkEmail(String email) {
         String RULE_EMAIL = "^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$";
-        //正则表达式的模式
-        Pattern p = Pattern.compile(RULE_EMAIL);
-        //正则表达式的匹配器
-        Matcher m = p.matcher(email);
-        //进行正则匹配
-        return m.matches();
+        return reMatch(RULE_EMAIL, email);
     }
     /**
      * 手机号匹配
      *
-     * @param str
+     * @param phone
      * @return
      */
-    public static boolean isMobile(String str) {
-        String s2 = "^[1](([3][0-9])|([4][5,7,9])|([5][0-9])|([6][6])|([7][3,5,6,7,8])|([8][0-9])|([9][8,9]))[0-9]{8}$";// 验证手机号
+    public static boolean isMobile(String phone) {
+        String RULE_PHONE = "^[1](([3][0-9])|([4][5,7,9])|([5][0-9])|([6][6])|([7][3,5,6,7,8])|([8][0-9])|([9][8,9]))[0-9]{8}$";// 验证手机号
+        return reMatch(RULE_PHONE, phone);
+    }
+
+    /**
+     * 通用匹配模板
+     * @param RULL_STR 正则规则字符串
+     * @param str 匹配的字符串
+     * @return
+     */
+    public static boolean reMatch(String RULL_STR, String str){
         //正则表达式的模式
-        Pattern p = Pattern.compile(s2);
+        Pattern p = Pattern.compile(RULL_STR);
         //正则表达式的匹配器
         Matcher m = p.matcher(str);
         //进行正则匹配
