86175ec20db6816c9a3b14b8670d87e64a38d1fa
wangjing <wj2856379842@163.com>
2020-08-21 09:06:37 +0800

    111

---
+++ src/com/pandas/learn/wj/studyfile/day20_08_21/goods/Person.java
@@ -0,0 +1,11 @@
+package com.pandas.learn.wj.studyfile.day20_08_21.goods;
+
+/**
+ * @author wangjing
+ * @create 2020-08-21 8:55
+ */
+public class Person {
+    private String name;
+    private static 
+
+}
---
+++ src/com/pandas/learn/wj/studyfile/day20_08_21/goods/Shop.java
@@ -0,0 +1,9 @@
+package com.pandas.learn.wj.studyfile.day20_08_21.goods;
+
+/**
+ * @author wangjing
+ * @create 2020-08-21 8:56
+ */
+public class Shop {
+    private String goodsName;
+}
